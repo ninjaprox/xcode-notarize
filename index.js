209325463efb570eeaf6dfa6613ab9f9d629384f
Vinh Nguyen <ninjaprox@gmail.com>
2022-11-11 16:57:28 +0800

    Use notarytool

--- index.js
+++ index.js
@@ -23,12 +23,6 @@ const fs = require('fs');
 
 const core = require('@actions/core');
 const execa = require('execa');
-const plist = require('plist');
-
-
-const sleep = (ms) => {
-    return new Promise(res => setTimeout(res, ms));
-};
 
 
 const parseConfiguration = () => {
@@ -81,7 +75,7 @@ const archive = async ({productPath}) => {
 };
 
 
-const submit = async ({productPath, archivePath, primaryBundleId, username, password, verbose}) => {
+const submit = async ({productPath, archivePath, verbose, ascAPIKeyID, ascAPIIssuer}) => {
     //
     // Make sure the product exists.
     //
@@ -90,45 +84,19 @@ const submit = async ({productPath, archivePath, primaryBundleId, username, pass
         throw Error(`No product could be found at ${productPath}`);
     }
 
-    //
-    // The notarization process requires us to submit a 'primary
-    // bundle id' - this is just a unique identifier for notarizing
-    // this specific product. If it is not provided then we simply
-    // use the actual bundle identifier from the Info.plist
-    //
-
-    if (primaryBundleId === "") {
-        const path = productPath + "/Contents/Info.plist";
-        if (fs.existsSync(path)) {
-            const info = plist.parse(fs.readFileSync(path, "utf8"));
-            primaryBundleId = info.CFBundleIdentifier;
-        }
-    }
-
-    if (primaryBundleId === null) {
-        throw Error("No primary-bundle-id set and could not determine bundle identifier from product.");
-    }
-
-    //
-    // Run altool to notarize this application. This only submits the
-    // application to the queue on Apple's server side. It does not
-    // actually tell us if the notarization was succesdful or not, for
-    // that we need to poll using the request UUID that is returned.
-    //
-
     const args = [
-        "altool",
-        "--output-format", "json",
-        "--notarize-app",
-        "-f", archivePath,
-        "--primary-bundle-id", primaryBundleId,
-        "-u", username,
-        "-p", password
-    ];
+        "notarytool", "submit",
+        "--key", "./appstore-connect-api-key",
+        "--key-id", ascAPIKeyID,
+        "--issuer", ascAPIIssuer,
+        "--wait",
+        "--timeout", "15m",
+    ]
 
     if (verbose === true) {
         args.push("--verbose");
     }
+    args.push(archivePath)
 
     let xcrun = execa("xcrun", args, {reject: false});
 
@@ -137,104 +105,7 @@ const submit = async ({productPath, archivePath, primaryBundleId, username, pass
         xcrun.stderr.pipe(process.stderr);
     }
 
-    const {exitCode, stdout, stderr} = await xcrun;
-
-    if (exitCode === undefined) {
-        // TODO Command did not run at all
-        throw Error("Unknown failure - altool did not run at all?");
-    }
-
-    if (exitCode !== 0) {
-        // TODO Maybe print stderr - see where that ends up in the output? console.log("STDERR", stderr);
-        const response = JSON.parse(stdout);
-        if (verbose === true) {
-            console.log(response);
-        }
-
-        for (const productError of response["product-errors"]) {
-            core.error(`${productError.code} - ${productError.message}`);
-        }
-        return null;
-    }
-
-    const response = JSON.parse(stdout);
-    if (verbose === true) {
-        console.log(response);
-    }
-
-    return response["notarization-upload"]["RequestUUID"];
-};
-
-
-const wait = async ({uuid, username, password, verbose}) => {
-    const args = [
-        "altool",
-        "--output-format", "json",
-        "--notarization-info",
-        uuid,
-        "-u", username,
-        "-p", password
-    ];
-
-    if (verbose === true) {
-        args.push("--verbose");
-    }
-
-    for (let i = 0; i < 10; i++) {
-        let xcrun = execa("xcrun", args, {reject: false});
-
-        if (verbose == true) {
-            xcrun.stdout.pipe(process.stdout);
-            xcrun.stderr.pipe(process.stderr);
-        }
-
-        const {exitCode, stdout, stderr} = await xcrun;
-
-        if (exitCode === undefined) {
-            // TODO Command did not run at all
-            throw Error("Unknown failure - altool did not run at all?");
-        }
-
-        if (exitCode !== 0) {
-            // TODO Maye print stderr - see where that ends up in the output? console.log("STDERR", stderr);
-            const response = JSON.parse(stdout);
-            if (verbose === true) {
-                console.log(response);
-            }
-
-            for (const productError of response["product-errors"]) {
-                core.error(`${productError.code} - ${productError.message}`);
-            }
-            return false;
-        }
-
-        const response = JSON.parse(stdout);
-        if (verbose === true) {
-            console.log(response);
-        }
-
-        const notarizationInfo = response["notarization-info"];
-        switch (notarizationInfo["Status"]) {
-            case "in progress":
-                core.info(`Notarization status <in progress>`);
-                break;
-            case "invalid":
-                core.error(`Notarization status <invalid> - ${notarizationInfo["Status Message"]}`);
-                return false;
-            case "success":
-                core.info(`Notarization status <success>`);
-                return true;
-            default:
-                core.error(`Notarization status <${notarizationInfo["Status"]}> - TODO`);
-                return false;
-        }
-
-        await sleep(30000);
-    }
-
-    core.error("Failed to get final notarization status on time.");
-
-    return false;
+    await xcrun;
 };
 
 const main = async () => {
@@ -254,29 +125,9 @@ const main = async () => {
             return;
         }
 
-        const uuid = await core.group('Submitting for Notarizing', async () => {
-            let uuid = await submit({archivePath: archivePath, ...configuration});
-            if (uuid !== null) {
-                core.info(`Submitted package for notarization. Request UUID is ${uuid}`);
-            }
-            return uuid;
+        await core.group('Submitting for Notarizing', async () => {
+            await submit({archivePath: archivePath, ...configuration});
         });
-
-        if (uuid == null) {
-            core.setFailed("Notarization failed");
-            return;
-        }
-
-        await sleep(15000); // TODO On a busy day, it can take a while before the build can be checked?
-
-        const success = await core.group('Waiting for Notarization Status', async () => {
-            return await wait({uuid: uuid, archivePath: archivePath, ...configuration})
-        });
-
-        if (success == false) {
-            core.setFailed("Notarization failed");
-            return;
-        }
 
         core.setOutput('product-path', configuration.productPath);
     } catch (error) {
